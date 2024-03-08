781756d8fd2791825bd3d36ab1cf8fdbb33a32d3
JuanParrado19 <juanaparrado19@gmail.com>
2024-03-08 03:09:32 -0500

    implement final docker-compose files

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,10 @@
 FROM node:18-alpine
 
 WORKDIR /app
-
+ENV PATH /app/node_modules/.bin:$PATH
 COPY package.json .
-
+COPY package-lock.json .
 RUN npm install
-
 COPY . .
-
-EXPOSE 8080
-
-CMD [ "npm", "run", "dev" ]
+EXPOSE 5173
+CMD ["npm", "run", "dev"]
--- vite.config.js
+++ vite.config.js
@@ -3,13 +3,20 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  preview: {
+    port: 5173,
+    strictPort: true,
+  },
   server: {
+    port: 5173,
+    strictPort: true,
+    host: true,
+    origin: 'http://localhost:5173',
     proxy: {
       '/api': {
-      target:'http://localhost:8090',
-      changeOrigin:true,
-      secure:false,
-      rewrite: (path) => path.replace(/^\/api/, '')
+        target: 'http://bff-pokeapi:8090',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
       },
     },
   },
